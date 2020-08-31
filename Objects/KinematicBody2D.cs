d20ed860987a50adbe1b4d676baf4d048c2a5015
sitiom <56180050+sitiom@users.noreply.github.com>
2020-08-31 01:55:25 +0000

    Refactor code and move movement to _PhysicsProcess

--- Objects/KinematicBody2D.cs
+++ Objects/KinematicBody2D.cs
@@ -1,72 +1,68 @@
 using Godot;
-using System;
-using System.Collections;
 
 public class KinematicBody2D : Godot.KinematicBody2D
 {
-	private Vector2 inputVector = Vector2.Zero;
-	private int SPEED = 150;
-	private bool isWalking = false;
-	private bool canDash = true;
-	private PackedScene bullet = ResourceLoader.Load("res://Objects/Bullet.tscn") as PackedScene;
-	private float bulletSpeed = 1000f;
-	private bool canShoot = true;
-	private float fireRate = 0.2f;
-	private int bulletCount = 50;
+    [Export] public int Speed = 150;
+    [Export] public float FireRate = 0.2f;
+    [Export] public int BulletCount = 50;
+    [Export] public float BulletSpeed = 1000f;
+	[Export] public PackedScene Bullet = ResourceLoader.Load("res://Objects/Bullet.tscn") as PackedScene;
+	private Vector2 _inputVector = Vector2.Zero;
+    private bool _isWalking;
+    private bool _canDash = true;
+    private bool _canShoot = true;
 
     public override void _Process(float delta)
     {
-        inputVector = GetInput();
-        Move();
+        // TODO: Future stuff here
     }
-	private Vector2 GetInput()
-	{
-		Vector2 velocity = new Vector2();
-		LookAt(GetGlobalMousePosition());
-		velocity.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
-		velocity.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
-		if(Input.IsActionJustPressed("ui_select") && canDash) Dash();
-		if (velocity.x != 0 || velocity.y != 0) isWalking = true;
-		if(Input.IsActionPressed("click") && canShoot && bulletCount > 0)
-		{
-			var bulletInstance = bullet.Instance() as RigidBody2D;
-			bulletInstance.Position = GetNode<Node2D>("Gun").GlobalPosition;
-			bulletInstance.RotationDegrees = RotationDegrees;
-			bulletInstance.ApplyImpulse(new Vector2(0,0), new Vector2(bulletSpeed, 0).Rotated(Rotation));
-			GetTree().Root.AddChild(bulletInstance);
-			bulletCount--;
-			UpdateBulletCount(bulletCount);
-			ShootTimer();
-		}
-		return velocity * SPEED;
-	}
-	private void Move()
-	{
-		inputVector = MoveAndSlide(inputVector);
-		isWalking = false;
-	}
-    
-	async public void Dash()
-	{
-		SPEED = 300;
-		await ToSignal(GetTree().CreateTimer(0.1f), "timeout");
-		SPEED = 150;
-		DashTimer();
-	}
-	async public void DashTimer()
-	{
-		canDash = false;
-		await ToSignal(GetTree().CreateTimer(3), "timeout");
-		canDash = true;
-	}
-	async public void ShootTimer()
-	{
-		canShoot = false;
-		await ToSignal(GetTree().CreateTimer(fireRate), "timeout");
-		canShoot = true;
-	}
-	public void UpdateBulletCount(int b)
-	{
-		GetNode<Label>("HUD/BulletCount").Text = "Number of Pewpew Left: " + b.ToString();
-	}
+
+    public override void _PhysicsProcess(float delta)
+    {
+        _inputVector = GetInput();
+        _inputVector = MoveAndSlide(_inputVector);
+    }
+    private Vector2 GetInput()
+    {
+        Vector2 velocity = new Vector2();
+        LookAt(GetGlobalMousePosition());
+        velocity.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+        velocity.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
+        if (Input.IsActionJustPressed("dash") && _canDash) Dash();
+
+        _isWalking = velocity.x != 0 || velocity.y != 0;
+
+        if (Input.IsActionPressed("click") && _canShoot && BulletCount > 0)
+        {
+            RigidBody2D bulletInstance = (RigidBody2D)Bullet.Instance();
+            bulletInstance.Position = GetNode<Node2D>("Gun").GlobalPosition;
+            bulletInstance.RotationDegrees = RotationDegrees;
+            bulletInstance.ApplyImpulse(new Vector2(0, 0), new Vector2(BulletSpeed, 0).Rotated(Rotation));
+            GetTree().Root.AddChild(bulletInstance);
+            BulletCount--;
+            GetNode<Label>("HUD/BulletCount").Text = $"Number of Bullets Left: {BulletCount}";
+            ShootTimer();
+        }
+        return velocity * Speed;
+    }
+
+    public async void Dash()
+    {
+        Speed = 300;
+        await ToSignal(GetTree().CreateTimer(0.1f), "timeout");
+        Speed = 150;
+        DashTimer();
+    }
+    public async void DashTimer()
+    {
+        _canDash = false;
+        await ToSignal(GetTree().CreateTimer(3), "timeout");
+        _canDash = true;
+    }
+    public async void ShootTimer()
+    {
+        _canShoot = false;
+        await ToSignal(GetTree().CreateTimer(FireRate), "timeout");
+        _canShoot = true;
+    }
 }
