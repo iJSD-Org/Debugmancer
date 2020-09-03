88ae3bfed5cb36364ba9bc2cf3657c3c89b30f91
sitiom <56180050+sitiom@users.noreply.github.com>
2020-09-03 14:27:05 +0000

    Implement TempEnemy2 behavior

--- Objects/TempEnemy2/TempEnemy2.cs
+++ Objects/TempEnemy2/TempEnemy2.cs
@@ -1,94 +1,133 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Debugmancer.Objects.TempEnemy1.States;
+using Debugmancer.Objects.TempEnemy2.States;
 using Godot;
 
 namespace Debugmancer.Objects.TempEnemy2
 {
 	public class TempEnemy2 : KinematicBody2D
 	{
+		[Signal]
+		public delegate void StateChanged();
+
+		public State CurrentState;
+		public Stack<State> StateStack = new Stack<State>();
+		public readonly Dictionary<string, Node> StatesMap = new Dictionary<string, Node>();
+
+
 		private readonly PackedScene _bulletScene = (PackedScene)ResourceLoader.Load("res://Objects/Bullets/EnemyBullet.tscn");
-		private int _shots;
-		private bool _canShoot = true;
-		private bool _burstStarted;
-		private readonly Timer _burstCoolDown = new Timer();
-		private readonly Timer _shotCoolDown = new Timer();
 		private KinematicBody2D _player;
+		private readonly Random _random = new Random();
+		private int _shots;
 
 		public override void _Ready()
 		{
-			_player = GetParent().GetNode("Player") as KinematicBody2D;
-			AddChild(_burstCoolDown);
-			AddChild(_shotCoolDown);
-			_burstCoolDown.Connect("timeout", this, "_on_Burst_timeout");
-			_shotCoolDown.Connect("timeout", this, "_on_Shot_timeout");
+			_player = GetParent().GetNode<KinematicBody2D>("Player");
+
+			StatesMap.Add("Chase", GetNode("States/Chase"));
+			StatesMap.Add("Stagger", GetNode("States/Stagger"));
+
+			CurrentState = (State)GetNode("States/Chase");
+
+			foreach (Node state in StatesMap.Values)
+			{
+				state.Connect(nameof(State.Finished), this, nameof(ChangeState));
+			}
+
 			GetNode("Health").Connect(nameof(Health.HealthChanged), this, nameof(OnHealthChanged));
+
+			GetNode<Timer>("ShootTimer").WaitTime = (float)(_random.NextDouble() * (.7 - .1) + .1);
+			GetNode<Timer>("ShootTimer").Start();
+
+			StateStack.Push((State)StatesMap["Chase"]);
+			ChangeState("Chase");
 		}
 
-		public override void _Process(float delta)
+		public override void _PhysicsProcess(float delta)
 		{
-			if (_shots == 20 && !_burstStarted)
-			{
-				StartBurstTimer();
-				_burstStarted = true;
-				_canShoot = false;
-			}
-
-			if (_canShoot)
-			{
-				StartShotTimer();
-			}
+			CurrentState.Update(this, delta);
 		}
 
-		private void StartBurstTimer()
+		private void ShootTimer_timeout()
 		{
-			Random waitTime = new Random();
-			_burstCoolDown.WaitTime = (float)(waitTime.NextDouble() * (2.5 - .95) + .95);
-			_burstCoolDown.Start();
-		}
-
-		private void StartShotTimer()
-		{
-			Random waitTime = new Random();
-			_canShoot = false;
-			_shotCoolDown.WaitTime = (float)(waitTime.NextDouble() * (.4 - .1) + .1);
-			_shotCoolDown.Start();
-		}
-		private void _on_Burst_timeout()
-		{
-			_burstCoolDown.Stop();
-			_shots = 0;
-			_canShoot = true;
-			_burstStarted = false;
-		}
-
-		private void _on_Shot_timeout()
-		{
-			_shotCoolDown.Stop();
-			_shots += 1;
-			SpawnBullet();
-		}
-
-		private void SpawnBullet()
-		{
-			_canShoot = true;
-			var bullet = (EnemyBullet)_bulletScene.Instance();
-			bullet.Speed = 130;
+			GetNode<Timer>("ShootTimer").Stop();
+			// Shoot
+			EnemyBullet bullet = (EnemyBullet)_bulletScene.Instance();
+			bullet.Speed = 135;
 			bullet.Position = Position;
 			bullet.Direction = new Vector2(_player.Position.x - Position.x, _player.Position.y - Position.y).Normalized();
 			GetParent().AddChild(bullet);
+			if (++_shots == 20)
+			{
+				_shots = 0;
+				GetNode<Timer>("ShootTimer").WaitTime = (float)(_random.NextDouble() * (2.5 - .95) + .95);
+				GetNode<Timer>("ShootTimer").Start();
+			}
+			else
+			{
+				GetNode<Timer>("ShootTimer").WaitTime = (float)(_random.NextDouble() * (.4 - .1) + .1);
+				GetNode<Timer>("ShootTimer").Start();
+			}
 		}
 
-		public void OnHealthChanged(int health)
-		{
-			if (health == 0)
-				QueueFree();
-		}
-
-		public void _on_Hitbox_body_entered(Area2D body)
+		public void Hitbox_BodyEntered(Area2D body)
 		{
 			Health health = (Health)GetNode("Health");
 			if (body.IsInGroup("playerBullet")) health.Damage(1);
 
-			if (body.IsInGroup("playerCritBullet")) health.Damage(2);
+			if (body.IsInGroup("playerCritBullet"))
+			{
+				health.Damage(2);
+				ChangeState("Stagger");
+			}
+		}
+
+		public async void OnHealthChanged(int health)
+		{
+			Modulate = Color.ColorN("Red");
+			await Task.Delay(100);
+			Modulate = new Color(1, 1, 1);
+			if (health == 0)
+				QueueFree();
+		}
+
+		private void ChangeState(string stateName)
+		{
+			CurrentState.Exit(this);
+			if (stateName == "Previous")
+			{
+				StateStack.Pop();
+			}
+			else if (stateName == "Dead")
+			{
+				QueueFree();
+				return;
+			}
+			else if (stateName == "Stagger")
+			{
+				StateStack.Push((State)StatesMap[stateName]);
+			}
+			else
+			{
+				StateStack.Pop();
+				StateStack.Push((State)StatesMap[stateName]);
+			}
+
+			CurrentState = StateStack.Peek();
+
+			// Pass target to Chase State
+			if (stateName == "Chase")
+			{
+				((Chase2)CurrentState).Init((Player.Player)_player);
+			}
+
+			// We don"t want to reinitialize the state if we"re going back to the previous state
+			if (stateName != "Previous")
+				CurrentState.Enter(this);
+
+			EmitSignal(nameof(TempEnemy2.StateChanged), CurrentState.Name);
 		}
 	}
 }
