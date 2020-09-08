6432ddd38beba2c22a83e5683a445ce0dee60db8
sitiom <56180050+sitiom@users.noreply.github.com>
2020-09-08 01:12:37 +0000

    Move Rich Presence to RichPresence Script

--- RichPresence.cs
+++ RichPresence.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Debugmancer.NamedPipeClient;
 using DiscordRPC;
 using Godot;
 
@@ -7,5 +8,37 @@ namespace Debugmancer
 	class RichPresence : Node
 	{
 		public DiscordRpcClient Client;
+
+		public override void _Ready()
+		{
+			InitRpc();
+		}
+
+		private void InitRpc()
+		{
+			RichPresence rpc = (RichPresence)GetNode("/root/RichPresence");
+
+			rpc.Client = new DiscordRpcClient("752376874339926187", -1, null, true, new UnityNamedPipe()) { ShutdownOnly = true };
+
+			rpc.Client.Initialize();
+
+			DiscordRPC.RichPresence rp = new DiscordRPC.RichPresence
+			{
+				Assets = new Assets()
+				{
+					SmallImageKey = "godot",
+					SmallImageText = "Made with Godot Engine",
+					LargeImageKey = "godot",
+					LargeImageText = "Debugmancer",
+				},
+				Timestamps = new Timestamps(DateTime.UtcNow)
+			};
+			rpc.Client.SetPresence(rp);
+		}
+
+		public override void _ExitTree()
+		{
+			Client.Dispose();
+		}
 	}
 }
