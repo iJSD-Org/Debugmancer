ae25f7f7538fb8fdb09e7c9372ab6d317034bf0d
sitiom <56180050+sitiom@users.noreply.github.com>
2020-09-09 05:48:22 +0000

    Target framework net48 & Refactor Code

--- RichPresence.cs
+++ RichPresence.cs
@@ -11,29 +11,22 @@ namespace Debugmancer
 
 		public override void _Ready()
 		{
-			InitRpc();
-		}
+			Client = new DiscordRpcClient("752376874339926187", -1, null, true, new UnityNamedPipe()) { ShutdownOnly = true };
 
-		private void InitRpc()
-		{
-			RichPresence rpc = (RichPresence)GetNode("/root/RichPresence");
-
-			rpc.Client = new DiscordRpcClient("752376874339926187", -1, null, true, new UnityNamedPipe()) { ShutdownOnly = true };
-
-			rpc.Client.Initialize();
+			Client.Initialize();
 
 			DiscordRPC.RichPresence rp = new DiscordRPC.RichPresence
 			{
+				Details = "https://github.com/sitiom/Debugmancer",
 				Assets = new Assets()
 				{
 					SmallImageKey = "godot",
 					SmallImageText = "Made with Godot Engine",
 					LargeImageKey = "godot",
-					LargeImageText = "Debugmancer",
 				},
 				Timestamps = new Timestamps(DateTime.UtcNow)
 			};
-			rpc.Client.SetPresence(rp);
+			Client.SetPresence(rp);
 		}
 
 		public override void _ExitTree()
