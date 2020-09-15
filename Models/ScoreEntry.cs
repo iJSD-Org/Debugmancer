445589b0aacd379677eb4b7febbd71934b926c17
sitiom <56180050+sitiom@users.noreply.github.com>
2020-09-15 07:14:35 +0000

    Add Newtonsoft.Json & ScoreEntry Model

---
+++ Models/ScoreEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Debugmancer.Models
+{
+	class ScoreEntry
+	{
+		public string Name { get; set; }
+		public int Score { get; set; }
+		public DateTime Date { get; set; }
+	}
+}
