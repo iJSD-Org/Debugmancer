f692dc4b30c029f6769ad7c63b10c98d7b3e821e
sitiom <56180050+sitiom@users.noreply.github.com>
2020-09-16 08:44:00 +0000

    Refactor code

--- Objects/Enemies/Void/States/Wander.cs
+++ Objects/Enemies/Void/States/Wander.cs
@@ -3,29 +3,27 @@ using System;
 
 
 namespace Debugmancer.Objects.Enemies.Void.States
-{   
-    public class Wander : State
-    {
-        [Export] public int Speed = 40;
-        private Vector2 _dir = new Vector2();
-        private Timer _wanderTimer;
+{
+	public class Wander : State
+	{
+		[Export] public int Speed = 40;
+		private Vector2 _dir;
+		private Timer _wanderTimer;
 
-        private Random _random = new Random();
-        public override void _Ready()
-        {
-             _wanderTimer = GetNode<Timer>("WanderTimer");
-        }
-        public void Init(Player.Entity target)
+		private readonly Random _random = new Random();
+
+		public override void _Ready()
 		{
-
+			_wanderTimer = GetNode<Timer>("WanderTimer");
 		}
 
 		public override void Enter(KinematicBody2D host)
 		{
 			_wanderTimer.Start();
-            _dir.x = _random.Next(-50,50);
-            _dir.y = _random.Next(-50,50);   
-        }
+			_dir.x = _random.Next(-50, 50);
+			_dir.y = _random.Next(-50, 50);
+		}
+
 		public override void Exit(KinematicBody2D host)
 		{
 			// Nothing to do here
@@ -38,14 +36,14 @@ namespace Debugmancer.Objects.Enemies.Void.States
 
 		public override void Update(KinematicBody2D host, float delta)
 		{
-            host.MoveAndSlide(_dir.Normalized() * Speed);
+			host.MoveAndSlide(_dir.Normalized() * Speed);
 		}
 
-        private void _on_WanderTimer_timeout()
-        {
-            _wanderTimer.Stop();
-            EmitSignal(nameof(Finished), "Idle");
-        }
+		private void _on_WanderTimer_timeout()
+		{
+			_wanderTimer.Stop();
+			EmitSignal(nameof(Finished), "Idle");
+		}
 
-    }   
+	}
 }
